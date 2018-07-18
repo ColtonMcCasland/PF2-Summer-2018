b804c11aaa3f0987ec5d22119de0bb2a5b0b8258
Markuspwn <36249316+Markuspwn@users.noreply.github.com>
2018-07-18 12:12:38 -0500

    added rank to main insert call.

--- A5.cpp
+++ A5.cpp
@@ -19,8 +19,8 @@
 using namespace std;
 //------------------------------------------------
 /*
-* Node Declaration
-*/
+ * Node Declaration
+ */
 //------------------------------------------------
 struct node
 {
@@ -37,150 +37,155 @@ struct node
 
 class pQueue
 {
-
+    
 private:
     node *front;
-
+    
 public:
     pQueue()
     {
         front = NULL;
     }
-
-//------------------------------------------------
-/*
-* Insert into time Queue
-*/
-//------------------------------------------------
-
-void insert( string name, int priority, char rank)
-{
+    
+    //------------------------------------------------
+    /*
+     * Insert into time Queue
+     */
+    //------------------------------------------------
+    
+    void insert( string name, int priority, int rank)
+    {
         node *tmp, *q;
-
+        
         //Temporary (new) Node to insert
         tmp = new node;
         tmp->name = name;
         tmp->priority = priority;
-
+        
         //Check if no nodes exist or if temp node Priority
         //is less than the queue's front node.
         //(Front -> Back is Low -> High Priority)
         if (front == NULL || (priority < front->priority && rank < front->rank))
-        {
-            tmp->next = front;
-            front = tmp;
-        }
-
-        else
-        {
-            q = front;
-
-            //Find Spot in Queue based on priority of node
-            while(q->next != NULL && priority >= q->next->priority  )
-                q = q->next;
-
+            {
+                tmp->next = front;
+                front = tmp;
+            }
+            
+            else
+            {
+                q = front;
+                
+                //Find Spot in Queue based on priority of node
+                while(q->next != NULL && priority >= q->next->priority)
+                    q = q->next;
+                
                 //Reassign ptrs
                 tmp->next = q->next;
                 q->next = tmp;
-
-        }
-}
-//------------------------------------------------
-/*
-* Delete from time Queue
-*/
-//------------------------------------------------
- //UNDER CONSTRUCTION///////////////////////////////////
-void del()
-    {
-        node *tmp;
-        //if empty print nothing
-        if(front == NULL)
-                cout<<"Queue underflow\n";
-
-        else
-        {
-            tmp = front;
-            cout << "Deleted item is: " << tmp->name << endl;
-            front = front->next;
-
-            free(tmp);
-        }
-    }
-//------------------------------------------------
-/*
-* Print Queue
-*/
-//------------------------------------------------
-void display()
-    {
-        node *ptr;
-
-        //pointers
-        ptr = front;
-
-        //if empty print nothing
-        if (front == NULL)
-            cout<<"Empty";
-
-        else
-        {
-            cout << "Queue is: \n";
-            cout << setw(5) << "Priority" << " | " << "Name\n";
-            while(ptr != NULL)
-            {
-                cout << setw(5) << ptr->priority << " | " << ptr->name << endl;
-
-                ptr = ptr->next;
+                
             }
         }
-    }
-};
-
-//------------------------------------------------
-/*
- * Main
- */
-//------------------------------------------------
-int main()
-{
-
-    int choice, priority, rank;
-    string name;
-    pQueue pq;
-    do
-    {
-        cout<<"1. Insert\n";
-        cout<<"2. Delete\n";
-        cout<<"3. Display\n";
-        cout<<"4. Quit\n";
-        cout<<"Enter your choice: ";
-        cin>>choice;
-        switch(choice)
+            
+            
+            //------------------------------------------------
+        /*
+         * Delete from time Queue
+         */
+            //------------------------------------------------
+            //UNDER CONSTRUCTION///////////////////////////////////
+            void del()
+            {
+                node *tmp;
+                //if empty print nothing
+                if(front == NULL)
+                    cout<<"Queue underflow\n";
+                
+                else
+                {
+                    tmp = front;
+                    cout << "Deleted item is: " << tmp->name << endl;
+                    front = front->next;
+                    
+                    free(tmp);
+                }
+            }
+            //------------------------------------------------
+        /*
+         * Print Queue
+         */
+            //------------------------------------------------
+            void display()
+            {
+                node *ptr;
+                
+                //pointers
+                ptr = front;
+                
+                //if empty print nothing
+                if (front == NULL)
+                    cout<<"Empty";
+                
+                else
+                {
+                    cout << "Queue is: \n";
+                    cout << setw(5) << "Priority" << " | " << "Name\n";
+                    while(ptr != NULL)
+                    {
+                        cout << setw(5) << ptr->priority << " | " << ptr->name << endl;
+                        
+                        ptr = ptr->next;
+                    }
+                }
+            }
+        };
+            
+            //------------------------------------------------
+        /*
+         * Main
+         */
+            //------------------------------------------------
+            int main()
         {
-            case 1:
-                cout<<"Input the name to be added in the queue : ";
-                cin>>name;
-                cout<<"Enter its priority : ";
-                cin>>priority;
-                pq.insert(name, priority);
-                break;
-
-            case 2:
-                pq.del();
-                break;
-
-            case 3:
-                pq.display();
-                break;
-
-            case 4:
-                break;
-
-            default :
-                cout<<"Wrong choice\n";
+            
+            int choice, priority,rank;
+            string name;
+            pQueue pq;
+            do
+            {
+                cout<<"1. Insert\n";
+                cout<<"2. Delete\n";
+                cout<<"3. Display\n";
+                cout<<"4. Quit\n";
+                cout<<"Enter your choice: ";
+                cin>>choice;
+                switch(choice)
+                {
+                    case 1:
+                        cout<<"Input the name to be added in the queue : ";
+                        cin >> name;
+                        cout<<"Enter its rank : ";
+                        cin >> rank;
+                        cout<<"Enter its priority : ";
+                        cin >> priority;
+                        pq.insert(name, priority, rank);
+                        break;
+                        
+                    case 2:
+                        pq.del();
+                        break;
+                        
+                    case 3:
+                        pq.display();
+                        break;
+                        
+                    case 4:
+                        break;
+                        
+                    default :
+                        cout<<"Wrong choice\n";
+                }
+            }
+            while(choice != 4);
+            return 0;
         }
-    }
-    while(choice != 4);
-    return 0;
-}
+
