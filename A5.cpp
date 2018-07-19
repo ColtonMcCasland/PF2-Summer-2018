fb36b39b62db444be550155487f0e3770b1b475a
JosephCantrell <39743346+JosephCantrell@users.noreply.github.com>
2018-07-19 12:44:34 -0500

    Added comments

--- A5.cpp
+++ A5.cpp
@@ -1,5 +1,6 @@
 // Name : Colton McCasland
 //        Walter Niblock
+//        Joseph Cantrell
 //
 // Assignment 5 (ADT(s) - Priority Queue Implementation)
 //------------------------------------------------
@@ -37,7 +38,6 @@ struct node
 
 class pQueue
 {
-
 private:
     node *front;
 
@@ -47,175 +47,182 @@ public:
         front = NULL;
     }
 
-    //------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------------------------
     /*
-     * Insert into time Queue
+     * Name: insert
      * Input: String name, Int Priority, Int rank
-     * Function:
+     * Return: Nothing
+     * Function: When called, inserts the new User by rank, then priority into the queue
      */
-    //------------------------------------------------
 
-    void insert( string name, int priority, int rank)
-    {
-        node *tmp, *q;
+void insert( string name, int priority, int rank)
+{
+    node *tmp, *q;
 
-        //Temporary (new) Node to insert
-        tmp = new node;
-        tmp->name = name;
-        tmp->priority = priority;
-        tmp->rank = rank;
+    //Temporary (new) Node to insert
+    tmp = new node;
+    tmp->name = name;
+    tmp->priority = priority;
+    tmp->rank = rank;
 
-        //Check if no nodes exist or if temp node Priority
-        //is less than the queue's front node.
-        //(Front -> Back is Low -> High Priority)
-        if (front == NULL || (priority <= front->priority && rank <= front->rank))
+    //Check if no nodes exist or if temp node Priority
+    //is less than the queue's front node.
+    //(Front -> Back is Low -> High Priority)
+    if (front == NULL || (priority <= front->priority && rank <= front->rank))
+        {
+            tmp->next = front;
+            front = tmp;
+        }
+
+        else
+        {
+            q = front;
+            //Find Spot in Queue based on Rank of node, Then priority
+            // If the current rank is lower than the next rank in line, continue
+            while(q->next != NULL && (rank >= q->next->rank))
             {
-                tmp->next = front;
-                front = tmp;
-            }
-
-            else
-            {
-                q = front;
-                //Find Spot in Queue based on Rank of node, Then priority
-                while(q->next != NULL && (rank >= q->next->rank))
+                // If the priority is less than the next priority in line, continue
+                //cout << "\nIn Rank Check\n";
+                if(priority > q->next->priority)
                 {
-                    //cout << "\nIn Rank Check\n";
-                    if(priority > q->next->priority)
-                    {
-                        //cout << "\nIn Priority Check\n";
-                        while(priority >= q->next->priority)
-                            q = q->next;
-                    }
-                    else
+                    // Same thing, just in a while loop
+                    //cout << "\nIn Priority Check\n";
+                    while(priority >= q->next->priority)
+                        // If both conditions are true, go to the next pointer
                         q = q->next;
                 }
-
-                //Reassign ptrs
-                tmp->next = q->next;
-                q->next = tmp;
-
+                // If only the rank is lower, go to the next pointer
+                else
+                    q = q->next;
             }
+
+            //Reassign ptrs
+            tmp->next = q->next;
+            q->next = tmp;
+
         }
+    }
 
+    //----------------------------------------------------------------------------------------------------------------------------
+    /*
+     * Name: del
+     * Input: Nothing
+     * Return: Nothing
+     * Function: When called, this deletes the top member of the queue
+     */
+void del()
+{
+    node *tmp;
+    //if empty print nothing
+    if(front == NULL)
+        cout<<"Queue underflow\n";
 
-            //------------------------------------------------
-        /*
-         * Delete from time Queue
-         */
-            //------------------------------------------------
-            //UNDER CONSTRUCTION///////////////////////////////////
-            void del()
-            {
-                node *tmp;
-                //if empty print nothing
-                if(front == NULL)
-                    cout<<"Queue underflow\n";
+    else
+    {
+        tmp = front;
+        cout << "Deleted item is: " << tmp->name << endl;
+        front = front->next;
 
-                else
-                {
-                    tmp = front;
-                    cout << "Deleted item is: " << tmp->name << endl;
-                    front = front->next;
+        free(tmp);
+    }
+}
 
-                    free(tmp);
-                }
-            }
-            //------------------------------------------------
-        /*
-         * Print Queue
-         */
-            //------------------------------------------------
-            void display()
-            {
-                node *ptr;
+    //----------------------------------------------------------------------------------------------------------------------------
+    /*
+     * Name: display
+     * Input: Nothing
+     * Return: Nothing
+     * Function: When called, Displays the entire queue sorted by rank, then priority
+     */
+void display()
+{
+    node *ptr;
 
-                //pointers
-                ptr = front;
+    //pointers
+    ptr = front;
 
-                //if empty print nothing
-                if (front == NULL)
-                    cout<<"Empty";
+    //if empty print nothing
+    if (front == NULL)
+        cout<<"Empty";
 
-                else
-                {
-                    cout << "Queue is: \n";
-                    cout << setw(5) << "Priority | " << "Name \t| " << "Rank\n";
-                    while(ptr != NULL)
-                    {
-                        cout << setw(5) << ptr->priority << "    | " << ptr->name << "\t | ";
-                        if(ptr->rank == 1)
-                            cout << "General" << endl;
-                        else if(ptr->rank == 2)
-                            cout << "Sergeant" << endl;
-                        else
-                            cout << "Private" << endl;
-
-                        ptr = ptr->next;
-                    }
-                }
-            }
-        };
-
-            //------------------------------------------------
-        /*
-         * Main
-         */
-            //------------------------------------------------
-            int main()
+    else
+    {
+        cout << "Queue is: \n";
+        cout << setw(5) << "Priority | " << "Name \t| " << "Rank\n";
+        while(ptr != NULL)
         {
+            cout << setw(5) << ptr->priority << "    | " << ptr->name << "\t | ";
+            if(ptr->rank == 1)
+                cout << "General" << endl;
+            else if(ptr->rank == 2)
+                cout << "Sergeant" << endl;
+            else
+                cout << "Private" << endl;
 
-            int choice, priority,rank;
-            string name;
-            pQueue pq;
-            do
-            {
-                cout<<"1. Insert\n";
-                cout<<"2. Delete\n";
-                cout<<"3. Display\n";
-                cout<<"4. Quit\n";
-                cout<<"Enter your choice: ";
-                cin>>choice;
-                switch(choice)
-                {
-                    case 1:
-                        cout<<"Input the name to be added in the queue : ";
-                        cin >> name;
-                        cout<<"Enter their rank : ";
-                        cout << "\n1: General\n2: Sergeant\n3: Private\n";
-                        cin >> rank;
-                        while(rank != 1 && rank != 2 && rank != 3)
-                        {
-                            cout << "That is not a rank, try again: ";
-                            cin >> rank;
-                        }
-                        //cin >> rank;
-                        cout<<"Enter their priority (1 - 5): ";
-                        cin >> priority;
-                        while(priority != 1 && priority != 2 && priority != 3 && priority != 4 && priority != 5 )
-                        {
-                            cout << "That is not an allowed Priority, Try again: ";
-                            cin >> priority;
-                        }
-                        pq.insert(name, priority, rank);
-                        break;
-
-                    case 2:
-                        pq.del();
-                        break;
-
-                    case 3:
-                        pq.display();
-                        break;
-
-                    case 4:
-                        break;
-
-                    default :
-                        cout<<"Wrong choice\n";
-                }
-            }
-            while(choice != 4);
-            return 0;
+            ptr = ptr->next;
         }
+    }
+}
+};
+
+    //----------------------------------------------------------------------------------------------------------------------------
+    /*
+     * Main
+     */
+
+int main()
+{
+    int choice, priority,rank;
+    string name;
+    pQueue pq;
+    do
+    {
+        cout<<"1. Insert\n";
+        cout<<"2. Delete\n";
+        cout<<"3. Display\n";
+        cout<<"4. Quit\n";
+        cout<<"Enter your choice: ";
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+                cout<<"Input the name to be added in the queue : ";
+                cin >> name;
+                cout<<"Enter their rank : ";
+                cout << "\n1: General\n2: Sergeant\n3: Private\n";
+                cin >> rank;
+                while(rank != 1 && rank != 2 && rank != 3)
+                {
+                    cout << "That is not a rank, try again: ";
+                    cin >> rank;
+                }
+                //cin >> rank;
+                cout<<"Enter their priority (1 - 5): ";
+                cin >> priority;
+                while(priority != 1 && priority != 2 && priority != 3 && priority != 4 && priority != 5 )
+                {
+                    cout << "That is not an allowed Priority, Try again: ";
+                    cin >> priority;
+                }
+                pq.insert(name, priority, rank);
+                break;
+
+            case 2:
+                pq.del();
+                break;
+
+            case 3:
+                pq.display();
+                break;
+
+            case 4:
+                break;
+
+            default :
+                cout<<"Wrong choice\n";
+        }
+    }
+    while(choice != 4);
+    return 0;
+}
 
